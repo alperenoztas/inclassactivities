fa0cdd2be60f85d63c51571d0fbe60c922cf9d73
Alperen Öztaş <alprnoztas@gmail.com>
2023-04-23 12:09:44 +0300

    Activity 6 JS Game has finished.

---
+++ Acitivity6/script.js
@@ -0,0 +1,53 @@
+/*
+@Author: Alperen Öztaş
+*/
+
+var button = document.getElementById("button");
+var level = 1;
+var clicks = 0;
+var timeout = 500;
+var intervalID = setInterval(changePosition, timeout);
+
+// change position of button randomly
+function changePosition() {
+
+    /* 
+    random position can be between 0 and 300 but it can be changed.
+    with window object we can get the width and height of the window.
+    but due to instructions, I didn't use it.
+    */
+
+    var marginTop = Math.random() * 300 + "px";
+    var marginLeft = Math.random() * 300 + "px";
+    button.style.marginTop = marginTop;
+    button.style.marginLeft = marginLeft;
+}
+
+
+function handleButtonClick(event) {
+
+    // if click has catched from event increase clicks
+    if (event.type === "click") {
+        clicks++;
+        //Level click and timeout log for debugging so anyone can see it
+        console.log("Level: " + level + "\nClicks: " + clicks + "\nTimeout: " + timeout);
+    }
+
+    // if user clicked button three times in a row, increase level, reset clicks, decrease timeout
+    if (clicks == 3) {
+        alert("Congratulations!\nYou passed level " + level + "!");
+        level++;
+        clicks = 0;
+        timeout -= 100;
+        if (timeout < 0) {
+            timeout = 0;
+        }
+        // clear interval so new interval can be set
+        clearInterval(intervalID); // stop the current interval
+        intervalID = setInterval(changePosition, timeout); // set up a new interval
+    }
+
+    changePosition();
+}
+
+button.addEventListener("click", handleButtonClick);
