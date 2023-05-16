1e3d3c3b0a100ea800a6f8c24c619654a7ed3ec4
Alperen Öztaş <alprnoztas@gmail.com>
2023-05-16 19:21:56 +0300

    Activity 8 has finished.

---
+++ Activity8/Oztas-Activity8.js
@@ -0,0 +1,42 @@
+/*
+@Author: Alperen Oztas
+*/
+
+var programmingLanguagesList = [
+    "Php",
+    "JavaScript",
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "Lisp",
+    "Perl",
+    "Python",
+    "JavaScript",
+    "Java",
+    "C",
+    "C++",
+    "Ruby",
+    "Scala",
+    "Kotlin",
+    "Swift",
+    "C#",
+    "Visual Basic",
+    "Go",
+    "R",
+    "Dart",
+    "Rust",
+]
+
+$(function () {
+
+    //Prevent user to enter future birthday
+    $("#birthday").datepicker({
+        maxDate : 0
+    });
+
+
+    //Autocomplete for programming languages
+    $("#programming_language").autocomplete({
+        source:programmingLanguagesList
+    })
+});
